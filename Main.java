495f66ca4cee67b4c9e2ae39a3a95bdcd158ca3b
LeonidLisin <leo_lis@mail.ru>
2019-08-02 00:24:57 +0300

    Задача про спираль

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.lang.reflect.InvocationTargetException;
-
-public class Main {
-    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException {
-        Tester.start(ClassUnderTest1.class);
-        Tester.start(ClassUnderTest2.class);
-        Tester.start(ClassUnderTest3.class);
-    }
-}
